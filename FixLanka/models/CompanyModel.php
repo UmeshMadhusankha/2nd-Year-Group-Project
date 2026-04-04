cbd31bf781481d4e16c96d41b322705078f4f638
Umesh_Yapa <umeshmadhusankha19@gmail.com>
2026-04-04 15:41:28 +0530

    fix: Ensure PDO connection is established in constructors of CompanyController and ProviderController

--- FixLanka/models/CompanyModel.php
+++ FixLanka/models/CompanyModel.php
@@ -6,6 +6,9 @@ class CompanyModel {
 
     public function __construct() {
         global $pdo;
+        if (!($pdo instanceof PDO)) {
+            $pdo = getDatabaseConnection();
+        }
         $this->pdo = $pdo;
     }
 
